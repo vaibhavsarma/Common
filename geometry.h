4d129b40c2fb25273e32e12e02c01eefc508da49
vaibhavsarma <vaibhavsarma@gmail.com>
2014-11-15 18:16:58 -0600

    commiting first file

---
+++ geometry.h
@@ -0,0 +1,24 @@
+
+class point{
+private: 
+	int x;
+	int y;
+
+public:
+	int getX();
+	int getY();
+	void setGeom(int x, int y);
+
+};
+
+class line{
+	private:
+	point  pts[2];
+public:
+
+
+	void setGeom(point A, point B)
+	{
+
+	}
+};
